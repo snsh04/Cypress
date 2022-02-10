8aee31dee51cad74115d4aa4f7f6b44e069336db
snehaRest <68036606+snehaRest@users.noreply.github.com>
2022-02-10 22:30:01 +1100

    More tests

--- cypress/integration/2-advanced-examples/sample.js
+++ cypress/integration/2-advanced-examples/sample.js
@@ -1,11 +1,19 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', () =>
   {
-      // first
-    it('Visit Ampion', () => 
+      // Open woolies page 
+    it('Visit Woolies', () => 
     {
       cy.visit("https://www.woolworths.com.au/");
     
       });
+        // 
+      it('Search for grapes', () => 
+      {
+       cy.get('.autocomplete-searchBar').type('grapes{enter}')
+      
+        });
     })
 
   
